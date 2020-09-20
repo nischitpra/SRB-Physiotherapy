170d3979ceb7e2df44b84ea1143178ac90272e9a
nischit <nischitpra@gmail.com>
2020-09-20 19:56:38 +0530

    Updates

---
+++ precache-manifest.640cf0fba79d8fe50953b6e3c1e6911c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "620832d094870fc9cd5efe9a3d4f206d",
+    "url": "/SRB-Physiotherapy/index.html"
+  },
+  {
+    "revision": "f9c915698be45859ea00",
+    "url": "/SRB-Physiotherapy/static/css/main.04c0abad.chunk.css"
+  },
+  {
+    "revision": "a9aa9eee71829c95527e",
+    "url": "/SRB-Physiotherapy/static/js/2.ca536145.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SRB-Physiotherapy/static/js/2.ca536145.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f9c915698be45859ea00",
+    "url": "/SRB-Physiotherapy/static/js/main.790e1bab.chunk.js"
+  },
+  {
+    "revision": "cc4f17d3a2dd62b16c72",
+    "url": "/SRB-Physiotherapy/static/js/runtime-main.2eedc2b0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SRB-Physiotherapy/static/media/HomePatientDoctor.5d5d9eef.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/SRB-Physiotherapy/static/media/logo.5d5d9eef.svg"
+  }
+]);
