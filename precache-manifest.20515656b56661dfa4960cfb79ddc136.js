dcd9b4d45e6584106296da4a78e1dd251cddd8ad
nischit <nischitpra@gmail.com>
2020-12-06 21:37:31 +0545

    Updates

---
+++ precache-manifest.20515656b56661dfa4960cfb79ddc136.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79a1e0c881be98f1196476d3548a78f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41d40b7685d8c3741318",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "13285e82c46a66b1386b",
+    "url": "/static/js/2.e8998416.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/static/js/2.e8998416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41d40b7685d8c3741318",
+    "url": "/static/js/main.6b9d5668.chunk.js"
+  },
+  {
+    "revision": "385219057d2d57e43f6a",
+    "url": "/static/js/runtime-main.149c04c2.js"
+  },
+  {
+    "revision": "ac7a6f14316ac67842bc3bf7a39e5f8f",
+    "url": "/static/media/1.ac7a6f14.JPG"
+  },
+  {
+    "revision": "cd11a005a742d2abb223a8e8d4d5843b",
+    "url": "/static/media/2.cd11a005.JPG"
+  },
+  {
+    "revision": "bbda2bb158711ecdd990bdc289eef92a",
+    "url": "/static/media/3.bbda2bb1.JPG"
+  },
+  {
+    "revision": "9ebc0b69e7890815851ce729129028e2",
+    "url": "/static/media/4.9ebc0b69.JPG"
+  },
+  {
+    "revision": "8556ca13062d74d4fc5ce68cf2a8f308",
+    "url": "/static/media/5.8556ca13.JPG"
+  },
+  {
+    "revision": "226484af252983967600cffec9daff06",
+    "url": "/static/media/6.226484af.JPG"
+  },
+  {
+    "revision": "89361c97b8940d6cbc076b7d19a8ac0e",
+    "url": "/static/media/7.89361c97.JPG"
+  },
+  {
+    "revision": "6d23233606a16b94662a533f80ead311",
+    "url": "/static/media/8.6d232336.JPG"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/HomePatientDoctor.5d5d9eef.svg"
+  },
+  {
+    "revision": "01b5443ff9d95e43df3f5e24aa32d141",
+    "url": "/static/media/logo.01b5443f.png"
+  }
+]);
